36b86608e89a2bcdced4f75a5f4aa3301d4d725d
Kelvin Muza <kelvinm@hyperiondev.com>
2024-08-05 21:52:04 +0200

    remove unused import

--- src/utils/requireAuth.js
+++ src/utils/requireAuth.js
@@ -1,12 +1,8 @@
-import { replace } from "react-router-dom";
-
-
 export async function requireAuth({ location }) {
-    const user = localStorage.getItem('user');
-    if (!user) {
-      console.log("Redirecting to login...");
-      window.location.href = "/login";
-    }
-    return null;
+  const user = localStorage.getItem("user");
+  if (!user) {
+    console.log("Redirecting to login...");
+    window.location.href = "/login";
   }
-  
+  return null;
+}
